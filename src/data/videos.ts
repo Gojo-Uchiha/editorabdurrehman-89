35fdc2f97861ce2830f7a21359e012eef6a43e69
Gojo-Uchiha <122119327+Gojo-Uchiha@users.noreply.github.com>
2025-01-25 10:52:15 +0500

    commit changes

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -62,8 +62,8 @@ export const videos: Video[] = [
     id: "7",
     title: "Popular Edit 3",
     category: "Popular Edits",
-    url: "https://drive.google.com/file/d/1B9GygcsRf3pm63vbuRPSNSZpTxnuV4lG/view?usp=sharing",
-    thumbnail: getDriveThumbnail("https://drive.google.com/file/d/1B9GygcsRf3pm63vbuRPSNSZpTxnuV4lG/view?usp=sharing")
+    url: "https://drive.google.com/file/d/1B9GygcsRf3pm63vbuRPSNSZpTxnuV4lG/view",
+    thumbnail: getDriveThumbnail("https://drive.google.com/file/d/1B9GygcsRf3pm63vbuRPSNSZpTxnuV4lG/view")
   },
   {
     id: "21",
